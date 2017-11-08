aa2870d6f49659b7c8c0a0a83be2daa62ebc2430
Alen Magdić <alen.magdic@fer.hr>
2017-11-08 23:25:42 +0100

    Dodano loadanje,pozicioniranje,renderiranje slike te napravljena i dodana GUI komponenta za prikaz praznih polja u koja se dovlače slova

---
+++ app/src/main/java/lumen/zpr/fer/hr/lumen/GameLayoutConstants.java
@@ -0,0 +1,16 @@
+package lumen.zpr.fer.hr.lumen;
+
+/**
+ * Sadrži sve konstante kojima se opisuje raspored layouta u igri. Sve konstante su opisane kao
+ * faktori koji se pri slaganju GUI-a množe sa dimenzijama displaya
+ */
+
+public class GameLayoutConstants {
+    static double IMAGE_MAX_WIDTH_FACTOR = 0.7;
+    static double IMAGE_MAX_HEIGHT_FACTOR = 0.4;
+    static double IMAGE_Y_COORDINATE_FACTOR = 0.1;
+    static double CHAR_FIELD_WIDTH_FACTOR = 1/25.0;
+    static double GAP_BETWEEN_CHAR_FIELDS_FACTOR = CHAR_FIELD_WIDTH_FACTOR/5.0;
+    static double CHAR_FIELDS_Y_COORDINATE_FACTOR = 0.55;
+
+}
--- app/src/main/java/lumen/zpr/fer/hr/lumen/menus/MainMenuActivity.java
+++ app/src/main/java/lumen/zpr/fer/hr/lumen/menus/MainMenuActivity.java
@@ -14,9 +14,6 @@ public class MainMenuActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-         //       WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_menu);
 
         Button startGameBtn = findViewById(R.id.pokreniIgru);
