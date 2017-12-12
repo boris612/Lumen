6f1d6b48de05c2f75c555df0336c860299493f65
DESKTOP-HI7VO1V\Marko <marko.sopcic@fer.hr>
2017-12-13 00:06:22 +0100

    Dodana randomizacija slova i pozicioniranje slova ispod boxova

--- app/src/main/java/lumen/zpr/fer/hr/lumen/GameLayoutConstants.java
+++ app/src/main/java/lumen/zpr/fer/hr/lumen/GameLayoutConstants.java
@@ -13,6 +13,7 @@ public class GameLayoutConstants {
     static double CHAR_FIELD_WIDTH_MAX_FACTOR = 0.08;
     static double CHAR_FIELDS_WIDTH_FACTOR = 0.9;
     static double CHAR_FIELDS_Y_COORDINATE_FACTOR = 0.55;
+    static double LETTER_IMAGE_Y_COORDINATE_FACTOR=0.85;
     static double CHAR_FIELD_GAP_WIDTH_TO_FIELD_WIDTH_FACTOR = 0.1;
 
     static double COIN_IMAGE_WIDTH_FACTOR = 0.1;
