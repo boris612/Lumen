ad10fd2f6a47cbbbcd6f9459f992781b811e28f1
FZgorelec <filip.zgorelec@fer.hr>
2018-12-02 18:31:27 +0100

    popravljen bug sa mnozenjem i permutacijama

--- domain/src/main/java/hr/fer/zpr/lumen/numbergame/generator/MultiplicationEquationGenerator.java
+++ domain/src/main/java/hr/fer/zpr/lumen/numbergame/generator/MultiplicationEquationGenerator.java
@@ -1,5 +1,9 @@
 package hr.fer.zpr.lumen.numbergame.generator;
 
+import hr.fer.zpr.lumen.numbergame.manager.NumberGameConstants;
+import hr.fer.zpr.lumen.numbergame.manager.NumberGameMode;
+import hr.fer.zpr.lumen.numbergame.manager.NumberGamePreferences;
+
 public class MultiplicationEquationGenerator extends EquationGenerator {
 
 
@@ -27,9 +31,14 @@ public class MultiplicationEquationGenerator extends EquationGenerator {
     }
 
     public void generateEquation() {
-
+        if(NumberGameConstants.numberGameMode.equals(NumberGameMode.PERMUTATIONS.name())){
+            firstNumber=numberGenerator.randomNumber((lowerLimit==0)?lowerLimit+2:lowerLimit, limitNumber);
+            secondNumber = numberGenerator.randomNumber(1, limitNumber);
+        }
+        else{
         firstNumber = numberGenerator.randomNumber(lowerLimit, limitNumber);
         secondNumber = numberGenerator.randomNumber(0, limitNumber);
+        }
         answer = firstNumber * secondNumber;
     }
 
